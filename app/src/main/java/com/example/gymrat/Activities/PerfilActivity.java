8c7a316ae75d80af9aca2de4f3a41a6cdfd8e3db
IVANUTP <garciafloresangel27@gmail.com>
2026-07-31 22:04:47 -0600

    31/7/26

--- app/src/main/java/com/example/gymrat/Activities/PerfilActivity.java
+++ app/src/main/java/com/example/gymrat/Activities/PerfilActivity.java
@@ -1,5 +1,6 @@
 package com.example.gymrat.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,11 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.gymrat.R;
 import com.example.gymrat.Utils.BottomNavHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.button.MaterialButton;
 
 public class PerfilActivity extends AppCompatActivity {
 
     private BottomNavigationView navigation;
-
+    private MaterialButton btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +21,24 @@ public class PerfilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_perfil);
 
         navigation = findViewById(R.id.bottomNavigation);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(v -> {
+
+            Intent intent = new Intent(
+                    PerfilActivity.this,
+                    LoginActivity.class
+            );
+
+            startActivity(intent);
+            finish();
+
+        });
 
         BottomNavHelper.setupBottomNav(
                 this,
                 navigation,
                 R.id.nav_perfil
         );
-
-
     }
-
 }
